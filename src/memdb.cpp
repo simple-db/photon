4e93f37a829f8829dbfeff71f5f3f331af0c85eb
wtzhuque <wtzhuque@163.com>
2019-03-05 10:28:21 +0800

    fix compile issue

--- src/memdb.cpp
+++ src/memdb.cpp
@@ -1,23 +1,8 @@
 //**********************************************************
-// File: memdb.h
+// File: memdb.cpp
 // Author: wtzhuque@163.com
 // Description:
 //**********************************************************
 
-#pragma once
-
 namespace photon {
-
-class MemDB {
-public:
-	MemDB();
-
-	~MemDB();
-
-	int init();
-
-	int destroy();
-}; // class MemDB
-
 } // namespace photon
-
